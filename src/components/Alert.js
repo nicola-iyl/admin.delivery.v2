936992e90965abd58853c5e19794754bf14a7770
nicola-iyl <nicola.tamburini@inyourlife.info>
2022-01-10 15:49:19 +0100

    commit

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context as MessageContext } from "../context/MessageContext";
 
 const Alert = ({message,type}) => {
+  const { clearMessages } = useContext( MessageContext );
   if (message === "") return null;
 
-  const alertType = `mb-0 alert alert-${type}`;
+  const alertType = `mb-0 alert alert-${type} alert-dismissible`;
   return ( 
     <div className="card mb-2 mt-2">
       <div className="card-body">
         <div className={ alertType } role="alert">
           { message }
+          <button type="button" className="btn-close" aria-label="Close" onClick={ () => clearMessages() }></button>
         </div>
       </div>
     </div>           
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,64 @@
 import React from "react";
-import MasterLayout from "../layouts/MasterLayout";
-import { faBell } from "@fortawesome/free-regular-svg-icons";
+import {
+  faAtom,
+  faUser,
+  faStore,
+  faCogs,
+  faTools
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+import MasterLayout from "../layouts/MasterLayout";
+import StandardPage from "../layouts/StandardPage";
 
 const Dashboard = () => {
   return (
       <MasterLayout>
-        <div>Dashboard</div>
-        <FontAwesomeIcon icon={faBell} />
+        <StandardPage title="Dashboard">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <div className="card">
+                        <div className="card-body">
+                          <FontAwesomeIcon className="w-100" size="6x" icon={ faUser }/>
+                        </div>
+                        <div className="card-title text-center"><Link className="nav-link" to="/users">UTENTI</Link></div>
+                      </div>                      
+                    </div>
+                    <div className="col-md-2">
+                      <div className="card">
+                        <div className="card-body">
+                          <FontAwesomeIcon className="w-100" size="6x" icon={ faStore }/>
+                        </div>
+                        <div className="card-title text-center"><Link className="nav-link" to="/shops">SHOPS</Link></div>
+                      </div>                      
+                    </div>
+                    <div className="col-md-2">
+                      <div className="card">
+                        <div className="card-body">
+                          <FontAwesomeIcon className="w-100" size="6x" icon={ faCogs }/>
+                        </div>
+                        <div className="card-title text-center"><Link className="nav-link" to="/services">SERVIZI</Link></div>
+                      </div>                      
+                    </div>
+                    <div className="col-md-2">
+                      <div className="card">
+                        <div className="card-body">
+                          <FontAwesomeIcon className="w-100" size="6x" icon={ faTools }/>
+                        </div>
+                        <div className="card-title text-center"><Link className="nav-link" to="/settings">CONFIGURAZIONI</Link></div>
+                      </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </StandardPage>
       </MasterLayout>
   );
 };
